77309235902b5fe0371a89914c46e8ec3007d2e3
tianxuewei1016 <93226539@qq.com>
2017-06-02 19:48:38 +0800

    Fragment的基类和各个子Fragment

---
+++ app/src/main/java/com/atguigu/shopping_0224/user/UserFragment.java
@@ -0,0 +1,39 @@
+package com.atguigu.shopping_0224.user;
+
+import android.graphics.Color;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.atguigu.shopping_0224.base.BaseFragment;
+
+/**
+ * 作者：田学伟 on 2017/6/2 19:47
+ * QQ：93226539
+ * 作用：用户Fragment
+ */
+
+public class UserFragment extends BaseFragment {
+    private TextView textView;
+
+    @Override
+    public View initView() {
+        textView = new TextView(mContext);
+        textView.setTextSize(20);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    /**
+     * 1.把数据绑定到控件上的时候，重新该方法
+     * 2.联网请求，把得到的数据绑定到视图上
+     */
+    @Override
+    public void initData() {
+        super.initData();
+        Log.e("TAG", "用户的数据被初始化了...");
+        textView.setText("用户内容");
+    }
+}
