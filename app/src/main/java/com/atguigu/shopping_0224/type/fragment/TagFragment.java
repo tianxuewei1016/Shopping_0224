fea110d9360b2ad084f5ceb142958bc827559062
tianxuewei1016 <93226539@qq.com>
2017-06-08 19:38:13 +0800

    创建ListFragment和TagFragemnt并且切换

---
+++ app/src/main/java/com/atguigu/shopping_0224/type/fragment/TagFragment.java
@@ -0,0 +1,32 @@
+package com.atguigu.shopping_0224.type.fragment;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.atguigu.shopping_0224.base.BaseFragment;
+
+/**
+ * 作者：田学伟 on 2017/6/8 19:29
+ * QQ：93226539
+ * 作用：
+ */
+
+public class TagFragment extends BaseFragment{
+    private TextView textView;
+    @Override
+    public View initView() {
+        textView = new TextView(mContext);
+        textView.setTextSize(20);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextColor(Color.RED);
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("TagFragment");
+    }
+}
--- app/src/main/java/com/atguigu/shopping_0224/type/fragment/TypeFragment.java
+++ app/src/main/java/com/atguigu/shopping_0224/type/fragment/TypeFragment.java
@@ -1,15 +1,19 @@
 package com.atguigu.shopping_0224.type.fragment;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.atguigu.shopping_0224.R;
+import com.atguigu.shopping_0224.activity.MainActivity;
 import com.atguigu.shopping_0224.base.BaseFragment;
 import com.flyco.tablayout.SegmentTabLayout;
 import com.flyco.tablayout.listener.OnTabSelectListener;
+
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -31,6 +35,12 @@ public class TypeFragment extends BaseFragment {
     FrameLayout flType;
     private String[] titles = {"分类", "标签"};
 
+    private ArrayList<BaseFragment> fragments;
+    /**
+     * 刚才被显示的fragment
+     */
+    private Fragment tempFragment;
+
     @Override
     public View initView() {
         View view = View.inflate(mContext, R.layout.fragment_type, null);
@@ -47,11 +57,25 @@ public class TypeFragment extends BaseFragment {
         super.initData();
 
         Log.e("TAG", "分类的数据被初始化了...");
+
+
+        initListener();
+        initFragment();
+    }
+
+    private void initFragment() {
+        fragments = new ArrayList<>();
+        fragments.add(new ListFragment());
+        fragments.add(new TagFragment());
+    }
+
+    private void initListener() {
         tl1.setTabData(titles);
         tl1.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelect(int position) {
-                Toast.makeText(mContext, "position==" + position, Toast.LENGTH_SHORT).show();
+//                Toast.makeText(mContext, "position==" + position, Toast.LENGTH_SHORT).show();
+                switchFragment(fragments.get(position));
             }
 
             @Override
@@ -59,6 +83,36 @@ public class TypeFragment extends BaseFragment {
 
             }
         });
+    }
+
+    private void switchFragment(BaseFragment currentFragment) {
+        //切换的不是同一个页面
+        if (tempFragment != currentFragment) {
+            MainActivity activity = (MainActivity) mContext;
+            //得到FragmentMager
+            FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
+            //如果没有九天假
+            if (!currentFragment.isAdded()) {
+                //缓存隐藏
+                if (tempFragment != null) {
+                    ft.hide(tempFragment);
+                }
+                //添加
+                ft.add(R.id.fl_type, currentFragment);
+            } else {
+                //缓存的隐藏
+                if (tempFragment != null) {
+                    ft.hide(tempFragment);
+                }
+                //显示
+                ft.show(currentFragment);
+            }
+            //事物提交
+            ft.commit();
+
+            //把当前的赋值缓存
+            tempFragment = currentFragment;
+        }
     }
 
     @Override
