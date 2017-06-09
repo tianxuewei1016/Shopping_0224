56855ade18617b2787ebafccff8cbb0feb946120
tianxuewei1016 <93226539@qq.com>
2017-06-09 21:45:38 +0800

    联网请求网络且解析数据

--- app/src/main/java/com/atguigu/shopping_0224/community/fragment/NewPostFragment.java
+++ app/src/main/java/com/atguigu/shopping_0224/community/fragment/NewPostFragment.java
@@ -1,11 +1,19 @@
 package com.atguigu.shopping_0224.community.fragment;
 
-import android.graphics.Color;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
 
+import com.alibaba.fastjson.JSON;
+import com.atguigu.shopping_0224.R;
 import com.atguigu.shopping_0224.base.BaseFragment;
+import com.atguigu.shopping_0224.community.bean.NewPostBean;
+import com.atguigu.shopping_0224.utils.Constants;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import okhttp3.Call;
 
 /**
  * 作者：田学伟 on 2017/6/9 21:08
@@ -14,15 +22,16 @@ import com.atguigu.shopping_0224.base.BaseFragment;
  */
 
 public class NewPostFragment extends BaseFragment {
-    private TextView textView;
+
+
+    @InjectView(R.id.lv_new_post)
+    ListView lvNewPost;
 
     @Override
     public View initView() {
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        View rootView = View.inflate(mContext, R.layout.fragment_news_post, null);
+        ButterKnife.inject(this, rootView);
+        return rootView;
     }
 
     /**
@@ -32,6 +41,32 @@ public class NewPostFragment extends BaseFragment {
     @Override
     public void initData() {
         super.initData();
-        textView.setText("热帖Fragment");
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        OkHttpUtils.get().url(Constants.NEW_POST_URL)
+                .id(100).build().execute(new StringCallback() {
+            @Override
+            public void onError(Call call, Exception e, int id) {
+//                Log.e("TAG", "联网失败==" + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(String response, int id) {
+//                Log.e("TAG", "新帖联网成功==");
+                processData(response);
+            }
+        });
+    }
+
+    private void processData(String json) {
+        NewPostBean bean = JSON.parseObject(json, NewPostBean.class);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.reset(this);
     }
 }
