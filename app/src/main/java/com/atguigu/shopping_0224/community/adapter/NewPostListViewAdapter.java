4e0f939d8c1b94d89165dc6bef506c52b82bf8a2
tianxuewei1016 <93226539@qq.com>
2017-06-10 12:51:52 +0800

    弹幕OpenDanmaku集成到NewPostFragemnt的适配器中

--- app/src/main/java/com/atguigu/shopping_0224/community/adapter/NewPostListViewAdapter.java
+++ app/src/main/java/com/atguigu/shopping_0224/community/adapter/NewPostListViewAdapter.java
@@ -13,7 +13,12 @@ import com.atguigu.shopping_0224.R;
 import com.atguigu.shopping_0224.community.bean.NewPostBean;
 import com.atguigu.shopping_0224.utils.Constants;
 import com.bumptech.glide.Glide;
+import com.opendanmaku.DanmakuItem;
+import com.opendanmaku.DanmakuView;
+import com.opendanmaku.IDanmakuItem;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -56,19 +61,43 @@ public class NewPostListViewAdapter extends BaseAdapter {
             convertView = View.inflate(mContext, R.layout.item_listview_newpost, null);
             viewHolder = new ViewHolder(convertView);
             convertView.setTag(viewHolder);
-        }else{
+        } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
         //根据位置得到对应的数据
-        NewPostBean.ResultEntity entity  = datas.get(position);
+        NewPostBean.ResultEntity entity = datas.get(position);
         //图片
-        Glide.with(mContext).load(Constants.BASE_URL_IMAGE+entity.getAvatar()).into(viewHolder.ibNewPostAvatar);
+        Glide.with(mContext).load(Constants.BASE_URL_IMAGE + entity.getAvatar()).into(viewHolder.ibNewPostAvatar);
 
         viewHolder.tvCommunityUsername.setText(entity.getUsername());
         viewHolder.tvCommunityLikes.setText(entity.getLikes());
         viewHolder.tvCommunityComments.setText(entity.getComments());
 
+        List<String> strings = entity.getComment_list();
+        if (strings != null && strings.size() > 0) {
+            //有弹幕数据
+            List<IDanmakuItem> list = initItems(viewHolder.danmakuView, strings);
+            Collections.shuffle(list);
+            viewHolder.danmakuView.addItem(list, true);
+            viewHolder.danmakuView.show();
+            viewHolder.danmakuView.setVisibility(View.VISIBLE);
+        } else {
+            viewHolder.danmakuView.hide();
+            viewHolder.danmakuView.setVisibility(View.GONE);
+        }
+
+
         return convertView;
+    }
+
+    private List<IDanmakuItem> initItems(DanmakuView danmakuView, List<String> strings) {
+        List<IDanmakuItem> list = new ArrayList<>();
+        for (int i = 0; i < strings.size(); i++) {
+            IDanmakuItem item = new DanmakuItem(mContext, strings.get(i), danmakuView.getWidth());
+            list.add(item);
+        }
+
+        return list;
     }
 
     static class ViewHolder {
@@ -88,6 +117,9 @@ public class NewPostListViewAdapter extends BaseAdapter {
         TextView tvCommunityLikes;
         @InjectView(R.id.tv_community_comments)
         TextView tvCommunityComments;
+
+        @InjectView(R.id.danmakuView)
+        DanmakuView danmakuView;
 
         ViewHolder(View view) {
             ButterKnife.inject(this, view);
